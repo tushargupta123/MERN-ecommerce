9885b506c8a82247fcff062c2fa0fb464e79cc29
tushargupta123 <tushargupta2k3@gmail.com>
2024-09-23 19:59:10 +0530

    client side deploy config

--- server/index.js
+++ server/index.js
@@ -20,11 +20,19 @@ const cartRouter = require('./routes/Cart');
 const ordersRouter = require('./routes/Order');
 const { User } = require('./model/User');
 const { Order } = require('./model/Order');
+const path = require('path');
 const { isAuth, sanitizeUser ,cookieExtractor} = require('./services/common');
 const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
+
+
+// Webhook
+
+// TODO: we will capture actual order after deploying out server live on public URL
+
 const endpointSecret = process.env.WEBHOOK_ENDPOINT;
 
+// where to run stripe cli (folderwhere stripe.exe downloaded in path type cmd.exe ->stripe.exe->stripe login)
 
 app.post('/webhook', express.raw({type: 'application/json'}), async(request, response) => {
 const sig = request.headers['stripe-signature'];
@@ -35,7 +43,7 @@ const sig = request.headers['stripe-signature'];
     event = await stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
   } catch (err) {
     console.log(err);
-    response.status(400).send(`Webhook Error: ${err.message}`);
+    response.status(400).send(Webhook Error: ${err.message});
     return;
   }
 
@@ -43,14 +51,17 @@ const sig = request.headers['stripe-signature'];
   switch (event.type) {
     case 'payment_intent.succeeded':
       const paymentIntentSucceeded = event.data.object;
+      // console.log({paymentIntentSucceeded})
       const order = await Order.findById(paymentIntentSucceeded.metadata.orderId);
       order.paymentStatus = 'received';
       await order.save()
       break;
+    // ... handle other event types
     default:
-      console.log(`Unhandled event type ${event.type}`);
+      console.log(Unhandled event type ${event.type});
   }
 
+  // Return a 200 response to acknowledge receipt of the event
   response.send();
 });
 
@@ -60,6 +71,8 @@ const opts = {};
 opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = process.env.JWT_SECRET_KEY; // TODO: should not be in code;
 
+//middlewares
+app.use(express.static(path.resolve(__dirname,'build')))
 app.use(cookieParser());
 app.use(
   session({
@@ -75,9 +88,9 @@ app.use(
   })
 );
 app.use(express.json()); // to parse req.body
-app.get('/',async(req,res)=>{
-  res.send("welcome to the quickmart-ecommerce backend")
-})
+// app.get('/',async(req,res)=>{
+//   res.send("welcome to the quickmart-ecommerce backend")
+// })
 app.use('/products', productsRouter.router);
 // we can also use JWT token for client-only auth
 app.use('/categories', categoriesRouter.router);
@@ -85,8 +98,11 @@ app.use('/brands', brandsRouter.router);
 app.use('/users', isAuth(), usersRouter.router);
 
 app.use('/auth', authRouter.router);
-app.use('/cart', isAuth(), cartRouter.router);
+app.use('/productCart', isAuth(), cartRouter.router);
 app.use('/orders', isAuth(), ordersRouter.router);
+
+// this line we add to make react router work in case of other routes doesnt match
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
 // Passport Strategies
 passport.use(
@@ -94,8 +110,11 @@ passport.use(
     new LocalStrategy(
       {usernameField:'email'},
       async function (email, password, done) {
+      // by default passport uses username
+      // console.log({email,password})
       try {
         const user = await User.findOne({ email: email });
+        // console.log(email, password, user);
         if (!user) {
           return done(null, false, { message: 'invalid credentials' }); // for safety
         }
@@ -122,6 +141,7 @@ passport.use(
       passport.use(
         'jwt',
         new JwtStrategy(opts, async function (jwt_payload, done) {
+          // console.log({ jwt_payload });
           try {
             const user = await User.findById(jwt_payload.id);
             if (user) {
@@ -135,12 +155,16 @@ passport.use(
         })
         );
         
+        
+// this creates session variable req.user on being called from callbacks
 passport.serializeUser(function (user, cb) {
+  // console.log('serialize', user);
   process.nextTick(function () {
     return cb(null, { id: user.id, role: user.role });
   });
 });
 
+// this changes session variable req.user when called from authorized request
 
 passport.deserializeUser(function (user, cb) {
   process.nextTick(function () {
@@ -177,7 +201,6 @@ app.post("/create-payment-intent", async (req, res) => {
     clientSecret: paymentIntent.client_secret,
   });
 });
-
 
 app.listen(process.env.PORT, async()=>{
     connectDB();
